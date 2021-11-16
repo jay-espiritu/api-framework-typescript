362804d82344caa407bd70974cb7be77b9fe77cf
Jay Espiritu <juan.aa.espiritu@gmail.com>
2021-11-16 19:28:05 +1100

    initial setup

---
+++ src/tests/utils.test.ts
@@ -0,0 +1,8 @@
+describe('utils', () => {
+  describe('utils#getMessage()', () => {
+    it('should return a message', () => {
+      const message = "Hello world!"
+      expect(message).toEqual('Hello world!');
+    });
+  });
+});
